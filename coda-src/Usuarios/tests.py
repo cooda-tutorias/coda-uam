a5f5bf4da136e4e01fd3a371c420a80f112ae67c
Antonio López Jaimes <alopez@cua.uam.mx>
2026-07-27 21:37:13 -0600

    fix(Usuarios): cambia campo cubiculo de IntegerField a CharField

--- coda-src/Usuarios/tests.py
+++ coda-src/Usuarios/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.db import models
+from Usuarios.models import Tutor, Cordinador, Coda  
 
-# Create your tests here.
+class CubiculoTestCase(TestCase):
+
+    def test_campo_cubiculo_es_charfield(self):
+        modelos = [Tutor, Cordinador, Coda]
+        
+        for modelo in modelos:
+            campo = modelo._meta.get_field('cubiculo')
+            # Verifica que el campo sea de tipo models.CharField
+            self.assertIsInstance(campo, models.CharField)
+
+            
+    def test_tipo_interno_cubiculo(self):
+            modelos = [Tutor, Cordinador, Coda]
+            
+            for modelo in modelos:
+                tipo = modelo._meta.get_field('cubiculo').get_internal_type()
+                # Compara la cadena de texto 'CharField'
+                self.assertEqual(tipo, 'CharField')
