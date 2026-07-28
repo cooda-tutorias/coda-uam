7734eaafaa38ad81ac36a91e8a73e2ff9ad47c1e
fredgolas <fredy00roca@gmail.com>
2026-07-28 15:56:04 -0600

    Fix default auth urls - Moved auth_views from Usuarios/urls.py to ssocial/urls.py - Added missing default urls for password reset.

--- coda-src/ssocial/urls.py
+++ coda-src/ssocial/urls.py
@@ -17,9 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Django default auth urls
+    path('logout/', auth_views.logout_then_login, name='logout'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('password-reset-confirm/<str:uidb64>/<str:token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
     path('', include('Tutorias.urls')),
     path('', include('Usuarios.urls')),
 ]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
